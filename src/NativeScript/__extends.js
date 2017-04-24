8461ea9a29f5f21dabdeebdf92ea985e75aaba5b
ivanbuhov <ivanbuhov@gmail.com>
2017-04-24 18:24:38 +0300

    Use Object.setPrototypeOf in ts extend function

--- src/NativeScript/__extends.js
+++ src/NativeScript/__extends.js
@@ -2,7 +2,7 @@
 // We inject our own function that branches logic whether base class is native or not.
 // This one gets called for classes that don't inherit from native ones.
 (function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    Object.setPrototypeOf(d, b);
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 });
